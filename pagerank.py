8db9af18c59c0953ed4975c1e565cdcd97352dbe
brycehills <hillsbryce@yahoo.com>
2020-05-23 14:28:25 -0700

    updated documentation

--- pagerank.py
+++ pagerank.py
@@ -15,28 +15,27 @@ change5=1
 
 iterations = 0 #track num iters
 
-#using convergence epsilon 0.001
+#using convergence epsilon 0.001 - forumlas depend on the d value given = 0.7 and contruction of graph nodes
 while change1 >= 0.001 and change2 >= 0.001 and change3 >= 0.001 and change5 >= 0.001:
 
-	rank1 = .15 + .7*(rank2)
-	change1 = abs(rank1 - temprank1)
-	temprank1 = rank1
+	rank1 = .15 + .7*(rank2) #compute PR
+	change1 = abs(rank1 - temprank1) # find change since last iteration
+	temprank1 = rank1 # hold previous ranking
 	
-	rank2 = .15 + .7*(rank1)
+	rank2 = .15 + .7*(rank1)#compute PR
+	change2 = abs(rank2 - temprank2)# find change since last iteration
+	temprank2 = rank2# hold previous ranking
 	
-	change2 = abs(rank2 - temprank2)
-	temprank2 = rank2
-	
-	rank3 = .15 + .7*(rank2/2)
-	change3 = abs(rank3 - temprank3)
-	temprank3 = rank3
+	rank3 = .15 + .7*(rank2/2) #compute PR
+	change3 = abs(rank3 - temprank3)# find change since last iteration
+	temprank3 = rank3# hold previous ranking
 
 	
-	rank5 = .15 + .7*(rank2/2 + rank5 + .06)
-	change5 = abs(rank5 - temprank5)
-	temprank5 = rank5
+	rank5 = .15 + .7*(rank2/2 + rank5 + .06) #compute PR
+	change5 = abs(rank5 - temprank5)# find change since last iteration
+	temprank5 = rank5# hold previous ranking
 	
-	iterations = iterations + 1
+	iterations = iterations + 1 # track iterations
 	
 print("Node 1 rank :", rank1)
 print("Node 2 rank :", rank1)
