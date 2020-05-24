7e70f75550b6f2493ca65c1340904b43fab26c7f
brycehills <hillsbryce@yahoo.com>
2020-05-23 18:53:04 -0700

    changed initial value and fixed output error

--- pagerank.py
+++ pagerank.py
@@ -18,27 +18,27 @@ iterations = 0 #track num iters
 #using convergence epsilon 0.001 - forumlas depend on the d value given = 0.7 and contruction of graph nodes
 while change1 >= 0.001 and change2 >= 0.001 and change3 >= 0.001 and change5 >= 0.001:
 
-	rank1 = .15 + .7*(rank2) #compute PR
+	rank1 = .06 + .7*(rank2) #compute PR
 	change1 = abs(rank1 - temprank1) # find change since last iteration
 	temprank1 = rank1 # hold previous ranking
 	
-	rank2 = .15 + .7*(rank1)#compute PR
+	rank2 = .06 + .7*(rank1)#compute PR
 	change2 = abs(rank2 - temprank2)# find change since last iteration
 	temprank2 = rank2# hold previous ranking
 	
-	rank3 = .15 + .7*(rank2/2) #compute PR
+	rank3 = .06 + .7*(rank2/2) #compute PR
 	change3 = abs(rank3 - temprank3)# find change since last iteration
 	temprank3 = rank3# hold previous ranking
 
 	
-	rank5 = .15 + .7*(rank2/2 + rank5 + .06) #compute PR
+	rank5 = .06 + .7*(rank2/2 + rank5 + .06) #compute PR
 	change5 = abs(rank5 - temprank5)# find change since last iteration
 	temprank5 = rank5# hold previous ranking
 	
 	iterations = iterations + 1 # track iterations
 	
 print("Node 1 rank :", rank1)
-print("Node 2 rank :", rank1)
-print("Node 3 rank :", rank1)
-print("Node 5 rank :", rank1)
+print("Node 2 rank :", rank2)
+print("Node 3 rank :", rank3)
+print("Node 5 rank :", rank5)
 print("Number of iterations :", iterations)
