dc700e976fc891b6888e8c69c592ebe06690503f
brycehills <hillsbryce@yahoo.com>
2020-05-29 20:25:35 -0700

    fixed lagging rank issue

--- pagerank.py
+++ pagerank.py
@@ -1,6 +1,7 @@
 rank1 = 1
 rank2 = 1
 rank3 = 1
+rank4 = 1
 rank5 = 1
 
 temprank1 = 0
@@ -18,27 +19,37 @@ iterations = 0 #track num iters
 #using convergence epsilon 0.001 - forumlas depend on the d value given = 0.7 and contruction of graph nodes
 while change1 >= 0.001 and change2 >= 0.001 and change3 >= 0.001 and change5 >= 0.001:
 
+	rank11 = rank1# save current rank
 	rank1 = .06 + .7*(rank2) #compute PR
 	change1 = abs(rank1 - temprank1) # find change since last iteration
 	temprank1 = rank1 # hold previous ranking
 	
-	rank2 = .06 + .7*(rank1)#compute PR
+	rank22 = rank2# save current rank
+	rank2 = .06 + .7*(rank11)#compute PR
 	change2 = abs(rank2 - temprank2)# find change since last iteration
 	temprank2 = rank2# hold previous ranking
 	
-	rank3 = .06 + .7*(rank2/2) #compute PR
+	rank33 = rank3# save current rank
+	rank3 = .06 + .7*(rank22/2) #compute PR
 	change3 = abs(rank3 - temprank3)# find change since last iteration
 	temprank3 = rank3# hold previous ranking
-
 	
-	rank5 = .06 + .7*(rank2/2 + rank5 + .06) #compute PR
+	rank44 = rank4# save current rank
+	rank4 = .06 + .7*(0) #compute PR
+
+
+	rank55 = rank5 # save current rank
+	rank5 = .06 + .7*(rank22/2 + rank55 + rank44) #compute PR
 	change5 = abs(rank5 - temprank5)# find change since last iteration
 	temprank5 = rank5# hold previous ranking
 	
 	iterations = iterations + 1 # track iterations
-	
+		
+print("FINAL OUTPUT:")
+print("----------------------------------")
 print("Node 1 rank :", rank1)
 print("Node 2 rank :", rank2)
 print("Node 3 rank :", rank3)
+print("Node 4 rank :", rank4)
 print("Node 5 rank :", rank5)
 print("Number of iterations :", iterations)
